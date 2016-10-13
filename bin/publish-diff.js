9cba5164f4338bd02b2e55c16e67db503e910f8c
Ryan Roemer <ryan@loose-bits.com>
2016-10-13 13:27:41 -0700

    Set up program structure more.

--- bin/publish-diff.js
+++ bin/publish-diff.js
@@ -1,6 +1,14 @@
 #!/usr/bin/env node
 "use strict";
 
-var args = require("../lib/args").parse();
+var opts = require("../lib/args").parse();
+var pubDiff = require("../lib/index").diff;
 
-console.log("TODO IMPLEMENT", args); // eslint-disable-line
+pubDiff(opts, function (err, diff) {
+  if (err) {
+    console.error(err.stack || err); // eslint-disable-line no-console
+    process.exit(1); // eslint-disable-line no-process-exit
+  }
+
+  console.log("TODO IMPLEMENT", diff); // eslint-disable-line
+});
