a69af033032975ff31624c1d0b7a19f710c1d37b
Ryan Roemer <ryan@loose-bits.com>
2016-10-17 15:58:55 -0700

    Convert more to final format and output actual diff.

--- bin/publish-diff.js
+++ bin/publish-diff.js
@@ -4,11 +4,20 @@
 var opts = require("../lib/args").parse();
 var pubDiff = require("../lib/index").diff;
 
-pubDiff(opts, function (err, diff) {
+pubDiff(opts, function (err, results) {
   if (err) {
     console.error(err.stack || err); // eslint-disable-line no-console
     process.exit(1); // eslint-disable-line no-process-exit
   }
 
-  console.log("TODO IMPLEMENT", diff); // eslint-disable-line
+  // Filter to changed diffs and display.
+  var output = Object.keys(results.diff)
+    // Convert to just text values
+    .map(function (k) { return results.diff[k]; })
+    // Keep non-empty diffs
+    .filter(function (d) { return d; })
+    // Convert to string
+    .join("\n");
+
+  process.stdout.write(output + "\n");
 });
